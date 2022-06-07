ad8e427aa62cea8d2c08e9fbcbe579e19146cf8a
LiamHepworth <104167628+LiamHepworth@users.noreply.github.com>
2022-06-07 20:57:02 +0100

    Creating new Cards
    Basic layout for card creation prior to implementing card data

--- Main.js
+++ Main.js
@@ -1,23 +1,15 @@
-let bookName = document.querySelectorAll('.book-title');
-let authorName = document.querySelectorAll('.author-name');
-let pagesRead = document.querySelectorAll('.pages-read');
-let totalPages = document.querySelectorAll('.total-pages');
-let hasBeenRead = document.querySelectorAll('.has-been-read');
+// let bookName = document.querySelectorAll('.book-title');
+// let authorName = document.querySelectorAll('.author-name');
+// let pagesRead = document.querySelectorAll('.pages-read');
+// let totalPages = document.querySelectorAll('.total-pages');
+// let hasBeenRead = document.querySelectorAll('.has-been-read');
+let bookCarousel = document.querySelector('.carousel')
 
 const addBook = document.querySelector('#add-book');
 const closePopUp = document.querySelector('#close-pop-up')
 const editBook = document.querySelectorAll('.delete-book');
 
 const addBookForm = document.querySelector('#add-book-form');
-
-
-addBook.addEventListener('mousedown', function(){
-    addBookForm.style.display = 'block';
-})
-
-closePopUp.addEventListener('mousedown', function(){
-    addBookForm.style.display = 'none';
-})
 
 let myLibrary = [{
     name: 'The Hobbit',
@@ -26,6 +18,16 @@ let myLibrary = [{
     pages: 295,
     isRead: false
 }];
+
+//opening and closing add book dialogue
+
+addBook.addEventListener('mousedown', function(){
+    addBookForm.style.display = 'block';
+})
+
+closePopUp.addEventListener('mousedown', function(){
+    addBookForm.style.display = 'none';
+})
 
 function Book(name, author, imageLink, pages, isRead) {
     this.name = name;
@@ -54,16 +56,35 @@ function isRead(){
     };
 };
 
-function clearForm(form, number){
-    for(let i = 0; i < number; i++){
+function createCardElement(){
+    const newBookCard = document.createElement('li');
+    newBookCard.className = 'book-card';
+    
+    const cardHeader = document.createElement('div');
+    newBookCard.appendChild(cardHeader);
+    
+    const bookTitle = document.createElement('h2');
+    const authorTitle = document.createElement('span');
+    const newEditButton = document.createElement('span');
+    cardHeader.append(bookTitle, authorTitle, newEditButton);
+    
+    const bookImage = document.createElement('img');
+    newBookCard.appendChild(bookImage);
+    
+    const bookInfo = document.createElement('div');
+    newBookCard.appendChild(bookInfo);
+    
+    const newBookPages = document.createElement('span');
+    const newBookHasBeenRead = document.createElement('span');
+    newBookCard.append(newBookPages, newBookHasBeenRead );
+    
+    bookCarousel.append(newBookCard);
+}
+
+createCardElement()
+
+function clearForm(form, numberOfArgs){
+    for(let i = 0; i < numberOfArgs; i++){
         form[i].value = '';
     }
 }
-
-function createCard(){
-    const newBookCard = document.createElement('li');
-    const cardHeader = document.createElement('div');
-    cardHeader.innerHTML = '<div><h2 class="book-title">The Hobbit</h2><span class="author-name">By J.R.R Tolkien</span></div><span class="material-symbols-outlined dark delete-book">edit_note</span>'
-    
-}
-
